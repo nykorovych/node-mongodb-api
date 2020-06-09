c056e45ceabc9cec3e97ac5305d5105184ba1222
Your Name <you@example.com>
2020-06-09 17:19:35 +0200

    bcryptjs added:

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const validator = require('validator')
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -23,8 +24,23 @@ const userSchema = mongoose.Schema({
   },
   passwordConfirm: {
     type: String,
-    require: [true, 'PUT passwordConfirm pepeg']
+    require: [true, 'PUT passwordConfirm pepeg'],
+    validate: {
+      // works ONLY on .create() and .save()
+      validator: function(el) {
+        return el === this.password;
+      }
+    }
   }
+});
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+
+  // **********************************INTERESTING that we dont persist the confiredpassword in db
+  // and required = true in our model means only tht this is a required field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
