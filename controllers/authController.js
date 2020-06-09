029b84c980a876e78fbce90ff70a7e19a9465b4b
Your Name <you@example.com>
2020-06-09 17:52:15 +0200

    jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,24 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const jwt = require('jsonwebtoken');
 
 exports.signup = catchAsync(async (req, res, next) => {
   console.log(req.body);
-  const newUser = await User.create(req.body);
+
+  //   now ONLY way to register an admin is through Atlas or created a special route
+  // this one no longer can register an admin
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm
+  });
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser
     }
